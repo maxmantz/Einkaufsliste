6cf8e07e8854a405c00d46a353f40c6e5c9d7394
ROY <baerenpole@googlemail.com>
2017-01-30 20:27:58 +0100

    Kassenmaschine hinzugefuegt

--- js/cachier.js
+++ js/cachier.js
@@ -9,3 +9,39 @@
 
   - man kann sich den Betrag in der Kasse anzeigen lassen
 */
+
+function KassenMaschine() {
+  var kasse = 0;
+
+
+  this.addCash = function (cash) {
+    kasse += cash;
+  }
+
+
+
+
+
+  this.removeCash = function (cash) {
+
+    if (kasse < cash){
+      console.log("Kassenbetrag zu niedrig!");
+      return;
+    }
+
+    kasse -= cash;
+  }
+
+
+  this.print = function(){
+    console.log(kasse);
+  }
+
+
+}
+
+var kassenMaschine = new KassenMaschine();
+kassenMaschine.addCash(1000);
+kassenMaschine.print();
+kassenMaschine.removeCash(400);
+kassenMaschine.print();
